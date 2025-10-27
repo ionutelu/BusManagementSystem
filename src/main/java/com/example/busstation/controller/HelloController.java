9737f3f2bec80e5e162898cca30f8513c74e2bd5
ionut <ionutpuiu010405@gmail.com>
2025-10-27 20:57:40 +0200

    initial commit

--- src/main/java/com/example/busstation/controller/HelloController.java
+++ src/main/java/com/example/busstation/controller/HelloController.java
@@ -2,8 +2,11 @@ package com.example.busstation.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
+@RequestMapping("/ajutor")
 @Controller
 public class HelloController {
     @GetMapping("/hello")
