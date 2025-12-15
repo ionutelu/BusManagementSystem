50fbfb50daa62861d585a6308f77017afcef2915
ionut <ionutpuiu010405@gmail.com>
2025-12-15 22:51:41 +0200

    Created filtering for BusTrips

--- src/main/java/com/example/busstation/repository/TripRepository.java
+++ src/main/java/com/example/busstation/repository/TripRepository.java
@@ -1,9 +1,30 @@
 package com.example.busstation.repository;
 import com.example.busstation.model.BusTrip;
+import com.example.busstation.model.BusTripStatus;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface TripRepository extends JpaRepository<BusTrip, Long> {
+
+    @Query("""
+    SELECT bt FROM BusTrip bt
+    WHERE (
+        :route IS NULL OR
+        LOWER(bt.route.origin.name) LIKE LOWER(CONCAT('%', :route, '%')) OR
+        LOWER(bt.route.destination.name) LIKE LOWER(CONCAT('%', :route, '%'))
+    )
+    AND (:status IS NULL OR bt.status = :status)
+""")
+    List<BusTrip> findFiltered(
+            @Param("route") String route,
+            @Param("status") BusTripStatus status,
+            Sort sort
+    );
 
 }
